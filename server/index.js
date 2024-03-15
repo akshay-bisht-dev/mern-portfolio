6e677c312c0fbef7b3964e2d6b4e0335f043455d
AKSHAY BISHT <akshay.bisht.dev@gmail.com>
2024-03-15 16:36:26 +0530

    Initial Commit MERN Portfolio Project

---
+++ server/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+
+require("./database/conn");
+const product_routes = require("./routes/productRoutes");
+
+app.get("/", (req, res) => {
+  res.send("Welcome Server...");
+});
+
+// Middleware or to set route
+app.use("/api", product_routes);
+
+app.listen(PORT, (req, res) => {
+  console.log("Server is Live...");
+});
